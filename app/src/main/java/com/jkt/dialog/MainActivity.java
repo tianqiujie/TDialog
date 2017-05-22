6d0e406d6f0729fd156c2b714e99c11d377e8381
姬开天 <jkt1992@163.com>
2017-05-22 10:51:32 +0800

    更改首页排版,添加用例和解释说明.

--- app/src/main/java/com/jkt/dialog/MainActivity.java
+++ app/src/main/java/com/jkt/dialog/MainActivity.java
@@ -7,24 +7,71 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements TDialog.onItemClickListener, TDialog.onDismissListener {
 
-    private TDialog mTDialog;
+    TDialog mTDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] contentArray = {"111","22","33"};
-        mTDialog = new TDialog(MainActivity.this, TDialog.Style.DownSheet, contentArray,
-                "自定义Dialog", "嘿嘿嘿", this);
-        mTDialog.setDismissListener(this);
-        mTDialog.setTitleColor(getResources().getColor(R.color.bgColor_overlay));
-        mTDialog.setMsgSize(18);
-        mTDialog.setContentColor(getResources().getColor(R.color.bgColor_overlay));
-        mTDialog.setCancelTextColor(getResources().getColor(R.color.colorAccent));
     }
 
     public void onClick(View view) {
-        mTDialog.show();
+        //当条目为2时候中间弹出方式改为横向两个条目
+        //中间弹出方式默认为点击外部区域可取消,底部弹出方式默认为不可取消
+        String[] contentArray = {"111", "22", "33"};
+        switch (view.getId()) {
+            case R.id.main_btn:
+                String[] array = {"取消", "确认"};
+                mTDialog = new TDialog(MainActivity.this, TDialog.Style.Center, array,
+                        "中间对话框", "点击外部区域不可取消", this);
+                mTDialog.setCancelable(false);
+                mTDialog.show();
+                break;
+            case R.id.main_btn1:
+                mTDialog = new TDialog(MainActivity.this, TDialog.Style.DownSheet, contentArray,
+                        "底部对话框", "点击外部区域不可取消", this);
+                mTDialog.show();
+                break;
+            case R.id.main_btn2:
+                mTDialog = new TDialog(MainActivity.this, TDialog.Style.Center, contentArray,
+                        "中间对话框", "点击外部区域可取消", this);
+                mTDialog.show();
+                break;
+            case R.id.main_btn3:
+                mTDialog = new TDialog(MainActivity.this, TDialog.Style.DownSheet, contentArray,
+                        "底部对话框", "点击外部区域可取消", this);
+                mTDialog.setCancelable(true);
+                mTDialog.show();
+                break;
+            case R.id.main_btn4:
+                mTDialog = new TDialog(MainActivity.this, TDialog.Style.Center, contentArray,
+                        "自定义样式,中间弹出", "点击外部区域可取消", this);
+                mTDialog.setContentColor(getResources().getColor(R.color.bgColor_overlay));
+                mTDialog.setMsgColor(getResources().getColor(R.color.colorAccent));
+                mTDialog.show();
+                break;
+            case R.id.main_btn5:
+                mTDialog = new TDialog(MainActivity.this, TDialog.Style.DownSheet, contentArray,
+                        "自定义样式,底部弹出", "点击外部区域不可取消", this);
+                mTDialog.setTitleColor(getResources().getColor(R.color.colorAccent));
+                mTDialog.setContentColor(getResources().getColor(R.color.colorAccent));
+                mTDialog.setMsgColor(getResources().getColor(R.color.bgColor_overlay));
+                mTDialog.show();
+                break;
+            case R.id.main_btn6:
+                mTDialog = new TDialog(MainActivity.this, TDialog.Style.Center, contentArray,
+                        "取消监听", "点击外部区域可取消", this);
+                mTDialog.setDismissListener(this);
+                mTDialog.show();
+                break;
+            case R.id.main_btn7:
+                mTDialog = new TDialog(MainActivity.this, TDialog.Style.DownSheet, contentArray,
+                        "取消监听", "点击外部区域可取消", this);
+                mTDialog.setCancelable(true);
+                mTDialog.setDismissListener(this);
+                mTDialog.show();
+                break;
+        }
     }
 
     @Override
